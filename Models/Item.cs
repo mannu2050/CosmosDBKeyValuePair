bee288dd6837eaeec100a462a2d757734d825160
Manish Sharma <mannu2050@gmail.com>
2018-02-21 13:47:04 +0530

    First check-in

---
+++ Models/Item.cs
@@ -0,0 +1,22 @@
+﻿namespace todo.Models
+{
+    using Microsoft.Azure.Documents;
+    using Newtonsoft.Json;
+
+    public class Item
+    {
+        public Item()
+        {
+            TimeToLive = 100; //100 seconds
+        }
+
+        [JsonProperty(PropertyName = "id")]
+        public string Id { get; set; }
+
+        [JsonProperty(PropertyName = "name")]
+        public string Name { get; set; }
+
+        [JsonProperty(PropertyName = "ttl", NullValueHandling = NullValueHandling.Ignore)]
+        public int? TimeToLive { get; set; }
+    }
+}
